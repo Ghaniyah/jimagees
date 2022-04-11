b2190e74a3cced72d073cdaeb04f88a76edb94c8
Ghaniyah <gabdulazeez2@gmail.com>
2022-04-11 21:26:32 +0100

    first commit

---
+++ fabrics/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+python manage.py migrate
+python manage.py createsuperuser
+
---
+++ fabrics/models.py
@@ -0,0 +1,7 @@
+import email
+from django.db import models
+from yaml import MappingStartEvent
+
+# Create your models here.
+python manage.py migrate
+
