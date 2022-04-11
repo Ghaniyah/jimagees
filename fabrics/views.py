afd2d4b94cf8b187fd26971abd1ff31b1e584c23
Ghaniyah <gabdulazeez2@gmail.com>
2022-04-11 22:22:55 +0100

    first project

--- fabrics/views.py
+++ fabrics/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-  
+# this is my first django project
 
 def index(response):
     return HttpResponse("<h1>jimagees fabrics</h1>")
